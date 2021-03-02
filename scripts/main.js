4d34e151c8a0d085505b6ae0a0c761ac26d8acfb
faizinkholiq <nasrunfaizinkholiq@gmail.com>
2021-03-02 14:36:33 +0700

    update counter

--- scripts/main.js
+++ scripts/main.js
@@ -18,4 +18,49 @@ window.onload = async () => {
       increasePoint(num);
     });
   }
+
+  let hexBtnHolder = document.getElementsByClassName("btn-holder");
+  hexBtnHolder[0].onclick = () => {
+    num++;
+    sendMsgToSW({ value: num });
+  };
+};
+
+const sendMsgToSW = (data = { msg: "hello" }) => {
+  if (navigator.serviceWorker.controller) {
+    navigator.serviceWorker.controller.postMessage(data);
+  } else {
+    window.location.reload();
+  }
+};
+
+const increasePoint = (num = 0) => {
+  document.getElementById("point").innerText = num;
+
+  // animation stuff
+  createPlusOneAnimation();
+};
+
+const createPlusOneAnimation = () => {
+  let div = document.createElement("div");
+
+  // fill text and location
+  let hexBtn = btn.getBoundingClientRect();
+  let x =
+    hexBtn.left +
+    hexBtn.width / 2 +
+    Math.pow(-1, num % 2) * ((Math.random() * hexBtn.width) / 2);
+  let y = hexBtn.top - Math.random() * 30;
+
+  div.innerText = "+1";
+  div.style.top = y + "px";
+  div.style.left = x + "px";
+
+  div.classList.add("anim");
+  document.body.appendChild(div);
+
+  // add event listener that will delete the div once the animation ends
+  div.addEventListener("animationend", () => {
+    div.parentNode.removeChild(div);
+  });
 };
